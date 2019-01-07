799ffdf932e5b17d765f1db2871c8d19b37ed15f
donalod <irldexter@podomere.com>
2019-01-07 19:20:36 +0000

    Still trying to get Algolia to go last but not working well

---
+++ pansift/themes/navigator-hugo/static/plugins/algolia/dist/js/initialise.js
@@ -0,0 +1,28 @@
+(function ($) {
+  if(document.getElementById("aa-input-nav-container") !== null){
+    //if ($('#aa-input-container').length) {
+      var al_client = algoliasearch('AJV2LM71WS', 'afe14bbaabe07eb4792c82ca6bda3449');
+      var al_index = al_client.initIndex('Skill');
+      //initialize autocomplete on search input (ID selector must match)
+
+      function al_autocomplete(){
+        autocomplete('#aa-search-nav',
+        { hint: true }, {
+          source: autocomplete.sources.hits(al_index, {hitsPerPage: 7}),
+          //value to be displayed in input control after user's suggestion selection
+          displayKey: 'name',
+          //hash of templates used when rendering dataset
+          templates: {
+            //'suggestion' templating function used to render a single suggestion
+            suggestion: function(suggestion) {
+              return '<span>' +
+                suggestion._highlightResult.name.value +'</span><span class="label label-success pull-right">Available</span>';
+            }
+          }
+          }).on('autocomplete:selected', function(event, suggestion, dataset) {
+          location.href = "https://client.pansift.com/clients/sign_up";
+        });;
+      }
+      al_autocomplete();
+    }
+})(jQuery);

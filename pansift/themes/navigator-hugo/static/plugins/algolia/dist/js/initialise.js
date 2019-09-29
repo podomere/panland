b71b29206344d8ccd2c42934a56746f8c67982f6
donalod <irldexter@podomere.com>
2019-09-29 16:32:46 +0100

    Updating for Algolia indexing of demo Canaries and also copy

--- pansift/themes/navigator-hugo/static/plugins/algolia/dist/js/initialise.js
+++ pansift/themes/navigator-hugo/static/plugins/algolia/dist/js/initialise.js
@@ -2,7 +2,7 @@
   if(document.getElementById("aa-input-nav-container") !== null){
     //if ($('#aa-input-container').length) {
       var al_client = algoliasearch('AJV2LM71WS', 'afe14bbaabe07eb4792c82ca6bda3449');
-      var al_index = al_client.initIndex('Skill');
+      var al_index = al_client.initIndex('Canary');
       //initialize autocomplete on search input (ID selector must match)
 
       function al_autocomplete(){
@@ -10,17 +10,17 @@
         { hint: true }, {
           source: autocomplete.sources.hits(al_index, {hitsPerPage: 7}),
           //value to be displayed in input control after user's suggestion selection
-          displayKey: 'name',
+          displayKey: 'aws_access_key',
           //hash of templates used when rendering dataset
           templates: {
             //'suggestion' templating function used to render a single suggestion
             suggestion: function(suggestion) {
               return '<span>' +
-                suggestion._highlightResult.name.value +'</span><span class="label label-success pull-right">Available</span>';
+                suggestion._highlightResult.name.value +'</span><span class="label label-success pull-right">'+suggestion._highlightResult.name.type_of+'</span>';
             }
           }
           }).on('autocomplete:selected', function(event, suggestion, dataset) {
-          location.href = "/#pracsims";
+          location.href = "/pricing";
         });;
       }
       al_autocomplete();

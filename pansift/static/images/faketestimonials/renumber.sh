39a90386a49417c0aa06ad920e8a1139099311ed
donalod <irldexter@podomere.com>
2019-10-12 17:50:48 +0100

    Lots of updates and some functionality for testimonials

--- pansift/static/images/faketestimonials/renumber.sh
+++ pansift/static/images/faketestimonials/renumber.sh
@@ -1,2 +1,3 @@
-ls -v *.jpeg | cat -n | while read n f; do mv -n "$f" "$n.jpeg"; done
-ls -v *.jpeg | cat -n | tail -n1
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+cd $DIR
+ls -v ./*.jpeg | cat -n | while read n f; do mv -n "$f" "$n.jpeg"; echo -e $n" Old:"$f".jpeg New:"$n".jpeg\r"; done

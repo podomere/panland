b593b2202d4a5572a5ec0252feeaf5f30e94793e
donalod <irldexter@podomere.com>
2021-07-15 18:16:00 +0100

    Trying the generic key

--- pansift/themes/navigator-hugo/static/plugins/algolia/dist/js/initialise.js
+++ pansift/themes/navigator-hugo/static/plugins/algolia/dist/js/initialise.js
@@ -1,7 +1,7 @@
 (function ($) {
   if(document.getElementById("aa-input-nav-container") !== null){
     //if ($('#aa-input-container').length) {
-      var al_client = algoliasearch('AJV2LM71WS', 'e8e0b30c4c2c7736513e195d1a91d0f9');
+      var al_client = algoliasearch('AJV2LM71WS', '5e6f9e73a96000a01f3f272b8509c15e');
       var al_index = al_client.initIndex('Agent_production');
       //initialize autocomplete on search input (ID selector must match)
     }

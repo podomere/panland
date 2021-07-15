c96553bb92650d67b4adc4c21a0e3dd4f937f583
donalod <irldexter@podomere.com>
2021-07-15 18:06:19 +0100

    Missing correct index in init

--- pansift/themes/navigator-hugo/static/plugins/algolia/dist/js/initialise.js
+++ pansift/themes/navigator-hugo/static/plugins/algolia/dist/js/initialise.js
@@ -1,8 +1,8 @@
 (function ($) {
   if(document.getElementById("aa-input-nav-container") !== null){
     //if ($('#aa-input-container').length) {
-      var al_client = algoliasearch('AJV2LM71WS', 'afe14bbaabe07eb4792c82ca6bda3449');
-      var al_index = al_client.initIndex('Canary');
+      var al_client = algoliasearch('AJV2LM71WS', 'e8e0b30c4c2c7736513e195d1a91d0f9');
+      var al_index = al_client.initIndex('Agent_production');
       //initialize autocomplete on search input (ID selector must match)
     }
 })(jQuery);
